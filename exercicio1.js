35bee632341c6570829cdfc8de3af65ac8df6234
Tiago4g <tiago.github@gmail.com>
2020-02-11 16:57:30 -0300

    exercicio 01

---
+++ exercicio1.js
@@ -0,0 +1,13 @@
+var endereco = {  
+  rua: "Rua dos pinheiros",
+  numero: 1293,
+  bairro: "Centro", 
+  cidade: "São Paulo",
+  uf: "SP"
+};
+
+function exerc1(endereco){
+  return ('O usuário mora em '+endereco.cidade+ ' / '+ endereco.bairro + ', no bairro '+endereco.cidade+ ', na rua '+endereco.rua+ 'com o nº '+endereco.numero+'.');
+}
+//O usuário mora em São Paulo / SP, no bairro Centro, na rua "Rua dos Pinheiros" com nº 1293.
+
